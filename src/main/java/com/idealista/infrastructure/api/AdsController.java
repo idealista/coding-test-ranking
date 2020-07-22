8ddc3379cb0bb715ada93c972e5c02074b275c04
Víctor M. Fernández García <victormfernandezgarcia@gmail.com>
2020-07-22 19:06:34 +0200

    Added Swagger

--- src/main/java/com/idealista/infrastructure/api/AdsController.java
+++ src/main/java/com/idealista/infrastructure/api/AdsController.java
@@ -3,26 +3,30 @@ package com.idealista.infrastructure.api;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AdsController {
 
-    //TODO añade url del endpoint
-    public ResponseEntity<List<QualityAd>> qualityListing() {
-        //TODO rellena el cuerpo del método
-        return ResponseEntity.notFound().build();
-    }
+  //TODO añade url del endpoint
+  @GetMapping(value = "/custom")
+  public ResponseEntity<List<QualityAd>> qualityListing() {
+    //TODO rellena el cuerpo del método
+    return ResponseEntity.notFound().build();
+  }
 
-    //TODO añade url del endpoint
-    public ResponseEntity<List<PublicAd>> publicListing() {
-        //TODO rellena el cuerpo del método
-        return ResponseEntity.notFound().build();
-    }
+  //TODO añade url del endpoint
+  @GetMapping(value = "/publicListing")
+  public ResponseEntity<List<PublicAd>> publicListing() {
+    //TODO rellena el cuerpo del método
+    return ResponseEntity.notFound().build();
+  }
 
-    //TODO añade url del endpoint
-    public ResponseEntity<Void> calculateScore() {
-        //TODO rellena el cuerpo del método
-        return ResponseEntity.notFound().build();
-    }
+  //TODO añade url del endpoint
+  @GetMapping(value = "/score")
+  public ResponseEntity<Void> calculateScore() {
+    //TODO rellena el cuerpo del método
+    return ResponseEntity.notFound().build();
+  }
 }
