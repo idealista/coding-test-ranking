3a67df775c861cd4983520951709dac2e8c127db
Víctor M. Fernández García <victormfernandezgarcia@gmail.com>
2020-07-22 22:55:26 +0200

    * Added documentation

--- src/main/java/com/idealista/infrastructure/api/AdsController.java
+++ src/main/java/com/idealista/infrastructure/api/AdsController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import com.idealista.application.service.AdService;
 import com.idealista.infrastructure.mapper.AdMapper;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +22,9 @@ public class AdsController {
 
   //  * Debes proporcionar 3 endpoints: Uno para calcular la puntuación de todos los anuncios,
   @GetMapping(value = "/ads/score")
+  @ApiOperation(value = "calculate score for ads")
+  @ApiResponses(value = {
+      @ApiResponse(code = 204, message = "No COntent")})
   public ResponseEntity<Void> calculateScore() {
     adService.calculateAdsWithScore();
     return ResponseEntity.noContent().build();
@@ -26,6 +32,10 @@ public class AdsController {
 
   //  otro para listar los anuncios para un usuario de idealista
   @GetMapping(value = "/ads/public")
+  @ApiOperation(value = "return public ads")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "OK"),
+      @ApiResponse(code = 404, message = "Not found")})
   public ResponseEntity<List<PublicAd>> publicListing() {
 
     List<PublicAd> result = adMapper.fromAdVoListToPublicAdList(adService.getPublicAds());
@@ -37,6 +47,10 @@ public class AdsController {
 
   //  otro para listar los anuncios para el responsable de del departamento de gestión de calidad.
   @GetMapping(value = "/ads/quality")
+  @ApiOperation(value = "return quality ads")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "OK"),
+      @ApiResponse(code = 404, message = "Not found")})
   public ResponseEntity<List<QualityAd>> qualityListing() {
     List<QualityAd> result = adMapper.fromAdVoListToQualityAdList(adService.getQualityAds());
     if (result.isEmpty()) {
