a1025be07d66ae0c01758ac2afbfac0deb62b6b1
athena-git <athena.ia@protonmail.com>
2020-09-30 18:35:49 +0200

    Feat: Adds Ad domain objects

---
+++ src/main/java/com/idealista/infrastructure/api/ad/search/domain/Ad.java
@@ -0,0 +1,67 @@
+package com.idealista.infrastructure.api.ad.search.domain;
+
+import com.idealista.infrastructure.persistence.Picture;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import static com.idealista.infrastructure.api.ad.search.domain.AdTypology.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Ad {
+
+    private Integer id;
+    private AdTypology typology;
+    private String description;
+    private List<Picture> pictures;
+    private Integer houseSize;
+    private Optional<Integer> gardenSize;
+    private Integer score;
+    private Optional<Date> irrelevantSince;
+    private AdQuality adQuality;
+
+    public boolean isAddIrrelevant(){
+        return this.irrelevantSince.isPresent();
+    }
+
+    public boolean isAddRelevant() {
+        return !this.isAddIrrelevant();
+    }
+
+    public boolean isFlat(){
+        return this.typology == FLAT;
+    }
+
+    public boolean isNotFlat(){
+        return !this.isFlat();
+    }
+
+    public boolean isChalet(){
+        return this.typology == CHALET;
+    }
+
+    public boolean isNotChalet(){
+        return !this.isChalet();
+    }
+
+    public boolean isGarage(){
+        return this.typology == GARAGE;
+    }
+
+    public boolean isNotGarage(){
+        return !this.isGarage();
+    }
+
+    public boolean isOfQuality(AdQuality adQuality){
+        return this.adQuality == adQuality;
+    }
+
+}
---
+++ src/main/java/com/idealista/infrastructure/api/ad/search/domain/AdQuality.java
@@ -0,0 +1,7 @@
+package com.idealista.infrastructure.api.ad.search.domain;
+
+public enum AdQuality {
+
+    RELEVANT, IRRELEVANT, NOT_SCORED
+
+}
