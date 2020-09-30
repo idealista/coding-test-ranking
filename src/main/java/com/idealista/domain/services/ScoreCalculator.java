02443c06286b4dc310727e2d5d5c1e68a6cab085
Daniel Masegosa <danielmasegosa@hotmail.com>
2020-09-30 16:55:57 +0200

    Implemented calculator for SD pictures

--- src/main/java/com/idealista/domain/services/ScoreCalculator.java
+++ src/main/java/com/idealista/domain/services/ScoreCalculator.java
@@ -5,12 +5,22 @@ import java.util.concurrent.atomic.AtomicInteger;
 public final class ScoreCalculator {
 
     public static final int PICTURE_HD_SCORE = 20;
+    public static final int PICTURE_SD_SCORE = 10;
 
     public Ad execute(Ad ad) {
         final AtomicInteger scoreCounter = new AtomicInteger();
-        ad.getPictures().forEach(p -> scoreCounter.getAndAdd(PICTURE_HD_SCORE));
+        ad.getPictures().forEach(p -> {
+            if (hasHighResolutionPicture(p)) {
+                scoreCounter.getAndAdd(PICTURE_HD_SCORE);
+            } else {
+                scoreCounter.getAndAdd(PICTURE_SD_SCORE);
+            }
+        });
         return ad.withScore(scoreCounter.get());
     }
 
+    private boolean hasHighResolutionPicture(Picture p) {
+        return p.getQuality().equals("HD");
+    }
 
 }
