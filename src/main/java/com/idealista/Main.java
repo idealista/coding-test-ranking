7ab86bf18bd82946b2171ea8892310783839d305
athena-git <athena.ia@protonmail.com>
2020-09-30 18:29:44 +0200

    Feat: Enables configuration properties

--- src/main/java/com/idealista/Main.java
+++ src/main/java/com/idealista/Main.java
@@ -2,10 +2,12 @@ package com.idealista;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class Main {
-    
+
     public static void main(String [] args){
         SpringApplication.run(Main.class, args);
     }
