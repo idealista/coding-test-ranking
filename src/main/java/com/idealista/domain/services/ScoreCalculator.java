e2579e49d1f19acd107e3ce0a8e2107cf251fcc7
Daniel Masegosa <danielmasegosa@hotmail.com>
2020-09-30 16:41:45 +0200

    Implemented calculator for multiple pictures

--- src/main/java/com/idealista/domain/services/ScoreCalculator.java
+++ src/main/java/com/idealista/domain/services/ScoreCalculator.java
@@ -1,8 +1,16 @@
 package com.idealista.domain.services;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public final class ScoreCalculator {
 
+    public static final int PICTURE_HD_SCORE = 20;
+
     public Ad execute(Ad ad) {
-        return ad.withScore(20);
+        final AtomicInteger scoreCounter = new AtomicInteger();
+        ad.getPictures().forEach(p -> scoreCounter.getAndAdd(PICTURE_HD_SCORE));
+        return ad.withScore(scoreCounter.get());
     }
+
+
 }
