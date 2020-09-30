45a34b7def9e4fed83ce01d5706e33fffd53ff9b
athena-git <athena.ia@protonmail.com>
2020-09-30 19:21:58 +0200

    Feat: Removes punctuation marks and spaces

--- src/main/java/com/idealista/infrastructure/utils/StringFormatterUtils.java
+++ src/main/java/com/idealista/infrastructure/utils/StringFormatterUtils.java
@@ -5,10 +5,25 @@ import java.text.Normalizer;
 public class StringFormatterUtils {
 
     private static final String ACCENT_UNICODE_REGEX = "\\p{M}";
+    private static final String NON_ALPHABETIC_CHARACTERS_REGEX = "[^a-zA-Z]";
+    private static final String ONE_OR_MORE_SPACES_REGEX = " +";
     private static final String EMPTY_CHARACTER = "";
+    private static final String SPACE_CHARACTER = " ";
 
+    /**
+     * Normalizes the input string by removing accents, converting it to lower case, replacing all non-alphabetic
+     * characters by spaces and all trimming spaces.
+     *
+     * @param input
+     * @return
+     */
     public static String normalize(String input){
         String normalizedKeyword = Normalizer.normalize(input, Normalizer.Form.NFD);
-        return normalizedKeyword.toLowerCase().replaceAll(ACCENT_UNICODE_REGEX, EMPTY_CHARACTER);
+        return normalizedKeyword
+            .toLowerCase()
+            .replaceAll(ACCENT_UNICODE_REGEX, EMPTY_CHARACTER)
+            .replaceAll(NON_ALPHABETIC_CHARACTERS_REGEX, SPACE_CHARACTER)
+            .replaceAll(ONE_OR_MORE_SPACES_REGEX, SPACE_CHARACTER)
+            .trim();
     }
 }
