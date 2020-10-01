6044c1dbde0bb4717978b8444881e96a327e618d
Daniel Masegosa <danielmasegosa@hotmail.com>
2020-10-01 20:13:57 +0200

    Updated repository data structure and added method to find all ads

--- src/main/java/com/idealista/infrastructure/persistence/InMemoryPersistence.java
+++ src/main/java/com/idealista/infrastructure/persistence/InMemoryPersistence.java
@@ -3,39 +3,50 @@ package com.idealista.infrastructure.persistence;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
 
 @Repository
 public class InMemoryPersistence {
 
-    private List<AdVO> ads;
-    private List<PictureVO> pictures;
+    private Map<Integer, AdVO> ads;
+    private Map<Integer,PictureVO> pictures;
 
     public InMemoryPersistence() {
-        ads = new ArrayList<AdVO>();
-        ads.add(new AdVO(1, "CHALET", "Este piso es una ganga, compra, compra, COMPRA!!!!!", Collections.<Integer>emptyList(), 300, null, null, null));
-        ads.add(new AdVO(2, "FLAT", "Nuevo ático céntrico recién reformado. No deje pasar la oportunidad y adquiera este ático de lujo", Arrays.asList(4), 300, null, null, null));
-        ads.add(new AdVO(3, "CHALET", "", Arrays.asList(2), 300, null, null, null));
-        ads.add(new AdVO(4, "FLAT", "Ático céntrico muy luminoso y recién reformado, parece nuevo", Arrays.asList(5), 300, null, null, null));
-        ads.add(new AdVO(5, "FLAT", "Pisazo,", Arrays.asList(3, 8), 300, null, null, null));
-        ads.add(new AdVO(6, "GARAGE", "", Arrays.asList(6), 300, null, null, null));
-        ads.add(new AdVO(7, "GARAGE", "Garaje en el centro de Albacete", Collections.<Integer>emptyList(), 300, null, null, null));
-        ads.add(new AdVO(8, "CHALET", "Maravilloso chalet situado en lAs afueras de un pequeño pueblo rural. El entorno es espectacular, las vistas magníficas. ¡Cómprelo ahora!", Arrays.asList(1, 7), 300, null, null, null));
+        ads = new HashMap<Integer, AdVO>() {
+            {
+                put(1, new AdVO(1, "CHALET", "Este piso es una ganga, compra, compra, COMPRA!!!!!", emptyList(), 300, null, null, null));
+                put(2, new AdVO(2, "FLAT", "Nuevo ático céntrico recién reformado. No deje pasar la oportunidad y adquiera este ático de lujo", singletonList(4), 300, null, null, null));
+                put(3, new AdVO(3, "CHALET", "", singletonList(2), 300, null, null, null));
+                put(4, new AdVO(4, "FLAT", "Ático céntrico muy luminoso y recién reformado, parece nuevo", singletonList(5), 300, null, null, null));
+                put(5, new AdVO(5, "FLAT", "Pisazo,", asList(3, 8), 300, null, null, null));
+                put(6, new AdVO(6, "GARAGE", "", singletonList(6), 300, null, null, null));
+                put(7, new AdVO(7, "GARAGE", "Garaje en el centro de Albacete", emptyList(), 300, null, null, null));
+                put(8, new AdVO(8, "CHALET", "Maravilloso chalet situado en lAs afueras de un pequeño pueblo rural. El entorno es espectacular, las vistas magníficas. ¡Cómprelo ahora!", asList(1, 7), 300, null, null, null));
+            }
+        };
 
-        pictures = new ArrayList<PictureVO>();
-        pictures.add(new PictureVO(1, "http://www.idealista.com/pictures/1", "SD"));
-        pictures.add(new PictureVO(2, "http://www.idealista.com/pictures/2", "HD"));
-        pictures.add(new PictureVO(3, "http://www.idealista.com/pictures/3", "SD"));
-        pictures.add(new PictureVO(4, "http://www.idealista.com/pictures/4", "HD"));
-        pictures.add(new PictureVO(5, "http://www.idealista.com/pictures/5", "SD"));
-        pictures.add(new PictureVO(6, "http://www.idealista.com/pictures/6", "SD"));
-        pictures.add(new PictureVO(7, "http://www.idealista.com/pictures/7", "SD"));
-        pictures.add(new PictureVO(8, "http://www.idealista.com/pictures/8", "HD"));
+        pictures = new HashMap<Integer, PictureVO>() {
+            {
+                put(1, new PictureVO(1, "http://www.idealista.com/pictures/1", "HD"));
+                put(2, new PictureVO(2, "http://www.idealista.com/pictures/2", "HD"));
+                put(3, new PictureVO(3, "http://www.idealista.com/pictures/3", "SD"));
+                put(4, new PictureVO(4, "http://www.idealista.com/pictures/4", "HD"));
+                put(5, new PictureVO(5, "http://www.idealista.com/pictures/5", "SD"));
+                put(6, new PictureVO(6, "http://www.idealista.com/pictures/6", "SD"));
+                put(7, new PictureVO(7, "http://www.idealista.com/pictures/7", "SD"));
+                put(8, new PictureVO(8, "http://www.idealista.com/pictures/8", "HD"));
+            }
+        };
     }
 
-    public List<AdVO> getAllAds() {
-        throw new UnsupportedOperationException("Not implemented");
+    public List<AdVO> findAllAds() {
+        return new ArrayList<>(ads.values());
     }
+
 }
